0fb939c42e3f8059fa221964696dbc1a9d047fb6
clarkshaeffer <clark.rov@gmail.com>
2024-03-11 16:05:42 -0700

    tabnext + cr fill

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -51,8 +51,9 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
 })
