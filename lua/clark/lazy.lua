496ae611d5e6f60abae216c5e96d97b2df701051
clarkshaeffer <clark.rov@gmail.com>
2024-04-17 15:24:23 -0700

    lazy update

--- lua/clark/lazy.lua
+++ lua/clark/lazy.lua
@@ -1,6 +1,6 @@
 require("lazy").setup({
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
 	{
