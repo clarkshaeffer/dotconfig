d5a96dce130871646bab562a2dfdcbdbc604942c
clarkshaeffer <clark.rov@gmail.com>
2024-10-24 15:55:52 -0700

    obsidian.nvim frontmatter + default filename

--- lua/clark/lazy.lua
+++ lua/clark/lazy.lua
@@ -59,6 +59,7 @@ require("lazy").setup({
       ft = { "markdown" },
     },
 
+
     -- comments
     { 'numToStr/Comment.nvim', opts = {}, lazy = false, },
 
@@ -74,14 +75,43 @@ require("lazy").setup({
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
-        -- opts = {
-        --     workspaces = {
-        --         {
-        --             name = "personal",
-        --             path = "~/vaults/personal",
-        --         },
-        --     },
-        -- },
+        opts = {
+            -- workspaces = {
+            --     {
+            --         name = "personal",
+            --         path = "path/to/dir",
+            --     },
+            -- },
+            note_frontmatter_func = function(note)
+                -- Add the title of the note as an alias.
+                if note.title then
+                    note:add_alias(note.title)
+                end
+
+                local out = {
+                    id = note.title,
+                    title = note.title,
+                    aliases = note.aliases,
+                    tags = note.tags,
+                    path = note.path,
+                    metadata = note.metadata,
+                }
+
+                -- `note.metadata` contains any manually added fields in the frontmatter.
+                -- So here we just make sure those fields are kept in the frontmatter.
+                if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+                    for k, v in pairs(note.metadata) do
+                        out[k] = v
+                    end
+                end
+
+                return out
+            end,
+
+            note_id_func = function(title)
+                return title
+            end,
+        },
     }
 
 })
